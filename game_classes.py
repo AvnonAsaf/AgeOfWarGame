d342360cea2cc2efb6f90b9bbfdcb530a0880a2d
danydance <92677005+danydance@users.noreply.github.com>
2022-12-22 13:52:59 +0200

    Update game_classes.py

--- game_classes.py
+++ game_classes.py
@@ -10,22 +10,35 @@ class Tower:
         self.color = (255, 00, 00)
         self.max_health = 1000
         self.damage = 0
-        
+        self._troops: list[Troop] = []
+    
+    def add_troop(self, troop_type):
+        troop = Troop(troop_type, self.screen)
+        self._troops.append(troop)
+    
     def draw_tower_ev_hp(self):  
         if self.side == "left":
-          font = pygame.font.Font(None, 36)
-          text = font.render(f"Health : {self.max_health - self.damage}", 1, (255, 255, 255))
-          self.screen.blit(text, (0, self.screen.get_height() - self.size - 30))
-          pygame.draw.rect(self.screen, self.color, (0, self.screen.get_height() - self.size,self.size, self.size))
+            font = pygame.font.Font(None, 36)
+            text = font.render(f"Health : {self.max_health - self.damage}", 1, (255, 255, 255))
+            self.screen.blit(text, (0, self.screen.get_height() - self.size - 30))
+            pygame.draw.rect(self.screen, self.color, (0, self.screen.get_height() - self.size,self.size, self.size))
+          
         else:
-          font = pygame.font.Font(None, 36)
-          text = font.render(f"Health : {self.max_health - self.damage}", 1, (255, 255, 255))
-          self.screen.blit(text, (self.screen.get_width() - self.size, self.screen.get_height() - self.size - 30))
-          pygame.draw.rect(self.screen, self.color, (self.screen.get_width() - self.size, self.screen.get_height() - self.size,self.size, self.size))
+            font = pygame.font.Font(None, 36)
+            text = font.render(f"Health : {self.max_health - self.damage}", 1, (255, 255, 255))
+            self.screen.blit(text, (self.screen.get_width() - self.size, self.screen.get_height() - self.size - 30))
+            pygame.draw.rect(self.screen, self.color, (self.screen.get_width() - self.size, self.screen.get_height() - self.size,self.size, self.size))
+        enemy = Troop("enemy", self.screen)
+        for troop in self._troops:
+            
+            troop.check_bump(self._troops, enemy)
+            troop.troop_move()
+            enemy.draw()
+            troop.draw()
+            
 
 
-    
-class Troops:
+class Troop:
     def __init__(self, troop_type, screen):
         self.troopsr_evolution = 0
         self.screen = screen
@@ -42,77 +55,75 @@ class Troops:
         if troop_type == "tank":
             self.size = 100
             self._init_tank()
-        if troop_type == "none":
-            self.size = 100
-            self._init_none()
+        if troop_type == "enemy":
+            self.size = 80
+            self._init_enemy()
     
     def _init_attacker(self):
         if self.troopsr_evolution == 0:
             self.health = 120
             self.damage = 70
-        size = 50
-        
-        self.x += size
-        
-        
+            self.vel = 2
+        self.x += self.size
+
     def _init_archer(self):
         if self.troopsr_evolution == 0:
             self.health = 80
             self.damage = 50
-        size = 30
-        self.x += size
+            self.vel = 3
+        self.x += self.size
     def _init_tank(self):
         if self.troopsr_evolution == 0:
             self.health = 200
             self.damage = 100
-        size = 100
-        self.x += size
-    def _init_none(self):
+            self.vel = 1
+        self.x += self.size
+    def _init_enemy(self):
         if self.troopsr_evolution == 0:
             self.health = 200
             self.damage = 100
-        size = 100
-        self.x += size
+            self.vel = 1
+        self.x = 1500
+   
     def troop_move(self):
-        if not self.bumped:
-            if self.troop_type == "attacker":
-                size = 50
-                pygame.draw.rect(self.screen, (00,00,00), (self.x, self.y - size, size, size))
-                self.x += 50
-                pygame.draw.rect(self.screen, (00,00,255), (self.x, self.y - size, size, size))
-                time.sleep(0.4)
-            elif self.troop_type == "archer":
-                size = 30
-                pygame.draw.rect(self.screen, (00,00,00), (self.x, self.y - size, size, size))
-                self.x += 50
-                pygame.draw.rect(self.screen, (00,142,150), (self.x, self.y - size, size, size))
-                time.sleep(0.2)
-            elif self.troop_type == "tank":
-                size = 100
-                pygame.draw.rect(self.screen, (00,00,00), (self.x, self.y - size, size, size))
-                self.x += 50
-                pygame.draw.rect(self.screen, (150,00,255), (self.x, self.y - size, size, size))
-                time.sleep(0.5)
-            elif self.troop_type == "none":
-                size = 100
-                pygame.draw.rect(self.screen, (00,00,00), (self.x, self.y - size, size, size))
-                self.x = 1400
-                pygame.draw.rect(self.screen, (150,00,255), (self.x, self.y - size, size, size))
-    def bump(self, other_troop):
-        """Check if the troop has bumped into another troop."""
-        # check if the x-coordinates of the two troops overlap
-        # if self.x + self.size >= other_troop.x - self.size - 20:
-        #     self.bumped = True  # set the bumped flag to True
-        #     return True
-        # else:
-        #     self.bumped = False
-        #     return False
-        if self.x < other_troop.x + other_troop.size + 100 and self.x + self.size + 100 > other_troop.x:
-            self.x = other_troop.x - self.size  # Stop the current troop
-            return True
-        self.bumped = False
-        return False
+        if self.bumped:
+            return
+        self.x += self.vel
+    
+    def attack(self, enemy):
+        enemy.health -= self.damage
+
         
-        
-        
+                
+    def draw(self):
+        size = self.size
+        font = pygame.font.Font(None, 30)
+        text = font.render(f"{self.health}", 1, (255, 255, 255))
+        self.screen.blit(text, (self.x, self.y - self.size - 30))
+        if self.troop_type == "attacker":
+            pygame.draw.rect(self.screen, (00,00,255), (self.x, self.y - size, size, size))
+        elif self.troop_type == "archer": 
+            pygame.draw.rect(self.screen, (00,142,150), (self.x, self.y - size, size, size))
+        elif self.troop_type == "tank":
+            pygame.draw.rect(self.screen, (150,00,255), (self.x, self.y - size, size, size))
+        elif self.troop_type == "enemy":
+            pygame.draw.rect(self.screen, (245,120,40), (self.x, self.y - size, size, size))
+    
+    def check_bump(self, troops, enemy_troop):
+        if len(troops) > 1:
+            for index in range(1, len(troops)):
+                if troops[index].x == self.x:
+                    if self.x >= troops[index - 1].x - self.size - 100:
+                        self.bumped = True
+                        break
+                    else:
+                        self.bumped = False
+        if troops[0].x >= enemy_troop.x - enemy_troop.size - 50:
+            troops[0].bumped = True
+            troops[0].attack(enemy_troop)
+        else:
+            troops[0].bumped = False
+   
+
+
 
